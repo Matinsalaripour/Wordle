830119aaa95551c3c91e9dd060951c0f1e15be5d
Matin <matinsalaripour84@gmail.com>
2025-03-21 13:40:55 +0330

    Added word guessing game logic

--- Mini.py
+++ Mini.py
@@ -1,19 +1,26 @@
 import random
 import string
 
-# Generate a random 6-letter word
 num = 6
 key = "".join(random.choice(string.ascii_lowercase) for _ in range(num))
-guesses = []
-colors = []
 
 # Allow the user to have six guesses
 for i in range(num):
-    guess = input(f"Enter guess {i+1} (6 letters): ").lower()
+    if i == 0:
+        suffix = "st"
+    elif i == 1:
+        suffix = "nd"
+    elif i == 2:
+        suffix = "rd"
+    else:
+        suffix = "th"
+    
+    guess = input(f"Enter {i+1}{suffix} guess (6 letters): ").lower()
+    
     while len(guess) != num or not guess.isalpha():
         guess = input("Invalid input. Enter a 6-letter word: ").lower()
-    guesses.append(guess)
-
+    
+    # Create a list of colors for the current guess
     color_row = []
     for j in range(num):
         if key[j] == guess[j]:
@@ -23,9 +30,7 @@ for i in range(num):
         else:
             color_row.append('R')
 
-    colors.append(color_row)
-
-    # Print the color feedback for the current guess
+    # Print the color feedback for the current guess    
     print("".join(color_row))
 
     # Check if the user has won
@@ -34,4 +39,4 @@ for i in range(num):
         break
 
 # Print the key at the end of the game
-print("Key:", key)
+print("key: ", key)
